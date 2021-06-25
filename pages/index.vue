1a8cf74f1fb210d50618b51e4d6b974e608b367c
adarshsi <sivaadarsh.in@gmail.com>
2021-06-25 09:27:48 -0400

    test push with nuxt

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app id="home" class="font" style="background-color: #252422">
+        <h1>This website is under heavy rebuild. Come back soon!</h1>
+        <h2>
+            <NuxtLink to="/test">Don't click me</NuxtLink>
+        </h2>
+    </v-app>
+</template>
+
+<style scoped>
+    .font {
+        font-family: 'Google Sans', cursive;
+        background-color: #252422
+    }
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app id="home" class="font" style="background-color: #252422">
+        <h1>You are not supposed to be here!!!</h1>
+        <h2>
+            <NuxtLink to="/">Take me back</NuxtLink>
+        </h2>
+    </v-app>
+</template>
+
+<style scoped>
+    .font {
+        font-family: 'Google Sans', cursive;
+        background-color: #252422
+    }
+</style>
